e0cc7f697ade1458cf20ab51b66c970a785b5d9c
eliasuriel <147642504+eliasuriel@users.noreply.github.com>
2024-06-07 09:43:30 -0600

    Create global_variables.py

---
+++ global_variables.py
@@ -0,0 +1,27 @@
+default_settings = False
+anio = ""
+directorio = ""
+logged = False
+hoja_prime = "Sheet1" 
+hoja_opx = "_RE_STD_CONTEXT_CLASS_REPORTING"
+
+black_list = ['aad bgsw-mx','acs sw dev bgsw-mx','sys eng bgsw-mx','aas csw bgsw-mx','aas dsw bgsw-mx',
+              'aas dcom bgsw-mx','aad bgsw-mx','aas asw bgsw-mx','aas tst bgsw-mx','0','project management bgsw-mx',
+              'aie tst bgsw-mx','acs tst bgsw-mx','acs po bgsw-mx','aas net bgsw-mx','aas tm bgsw-mx', 'aas pi bgsw-mx',
+              'aas bsw-csw beg-na','eng-bgsw-mx','others bgsw-mx','aas swpcm bgsw-mx','hw-pjm bgsw-mx', 'mx eds frontend-na', 
+              'aad tm bgsw-mx', 'ass tst bgsw-mx','acs bgsw-mx','ass swa bgsw-mx', 'ass bsw bgsw-mx', 'ase bgsw-mx','aie tpm bgsw-mx']
+
+projects_display = ["BRP", "Zoox", "Faraday", "Tesla", "Ford", "Singer", "Harley Davidson",
+                    "GM", "Lordstwon", "Oshkosh", "Lucid", "Navistar", "Rexroth", "METALSA", "ASNA", "ReCar", "Re:Car", "Fisker", "Battle_Motors", 
+                    "BYD", "Falcon", "Motiv", "FCCC", "Support"]
+
+componentes = ["CSW", "NET", "DCOM", "DSW", "TST", "SYS", "ASW","BSW","REQ","CSW"] 
+
+
+user_credentials = [
+    ["", ""],
+    ["u2", "p2"],
+    ["u3", "p3"],
+    ["u4", "p4"],
+    ["u5", "p5"]
+]
